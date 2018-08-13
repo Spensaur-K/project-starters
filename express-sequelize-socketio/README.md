9554ed95a776f05c144d1eafc1ad71d6b679b85b
Spencer Killen <sjkillen@ualberta.ca>
2018-08-12 18:40:45 -0600

    Add express-sequelize-socketio

---
+++ express-sequelize-socketio/README.md
@@ -0,0 +1,1 @@
+# squeares-everywhere
---
+++ express-sequelize-socketio/app.js
@@ -0,0 +1,93 @@
+const express = require("express"),
+    body = require("body-parser"),
+    Sequelize = require("sequelize"),
+    path = require("path"),
+    helmet = require("helmet"),
+    http = require("http"),
+    iscolor = require("is-color"),
+    socketio = require("socket.io"),
+    dotenv = require("dotenv");
+
+dotenv.config();
+const { PORT } = process.env;
+
+const app = express();
+const server = new http.Server(app);
+const io = socketio(server);
+
+app.use(body.json());
+app.use(helmet());
+
+const db = new Sequelize("app", null, null, {
+    host: "localhost",
+    dialect: "sqlite",
+    storage: path.join(__dirname, "data"),
+});
+
+const Tile = db.define("Tile", {
+    x: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+    },
+    y: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+    },
+    color: {
+        type: Sequelize.STRING,
+    },
+});
+
+db.sync({});
+
+class FriendlyError extends Error {
+    constructor(friendly, message = friendly) {
+        super(message);
+        this.friendly = friendly;
+    }
+    toString() {
+        return this.friendly + "\n" + super.toString();
+    }
+}
+
+app.use(express.static(path.join(__dirname, "client")));
+
+app.post("/tile", ({ body: { x, y, color } }, res, next) => {
+    if ((typeof x === undefined) || (typeof y === undefined)) {
+        return next(new FriendlyError("Missing x or y"));
+    }
+    if (!color) {
+        return next(new FriendlyError("Missing color"));
+    }
+    if (!iscolor(color)) {
+        return next(new FriendlyError("Invalid color"));
+    }
+    Tile.upsert({
+        x, y, color,
+    }).then(() => {
+        io.sockets.emit("tile", { x, y, color });
+        res.status(200).end();
+    }, next);
+});
+
+app.get("/tiles", (req, res, next) => {
+    Tile.findAll({ attributes: ["x", "y", "color"], where: {} }).then(tiles => res.status(200).json(tiles), next);
+});
+
+app.get("*", (req, res, next) => {
+    res.status(404).send("Not Found").end();
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    const text = err.friendly || "Something went wrong...";
+    res.status(500).json(text);
+});
+
+io.on("connection", socket => {
+
+});
+
+server.listen(PORT, () => {
+    console.log(`HTTP Server listening on port "${PORT}"`);
+});
