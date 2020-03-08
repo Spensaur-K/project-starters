ef38326703a38faf340b4b34a37bc66e58df2f66
Spencer Killen <sjkillen@ualberta.ca>
2020-03-08 14:43:48 -0600

    Version bump

--- webpack-ts-react-babel-d3-sass/update.js
+++ webpack-ts-react-babel-d3-sass/update.js
@@ -5,6 +5,8 @@
 const pack = require("./package.json");
 const deps = Object.keys(pack.devDependencies);
 
+console.log("Run the following command manually: \n");
+
 console.log(`npm install --save-dev ${deps.join(" ")}`);
 
 const fs = require("fs");
